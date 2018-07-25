4706fed844150ae5806e8b0ad76bca70504a8d53
Simon Albani <simon.albani2018@gmail.com>
2018-07-25 16:16:28 -0400

    comments

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-//checks if the the letter provided by the user matches one or more of the letters in the word
+//variable to keep count of how many tries are left
 let triesLeft = 6;
+//checks if the the letter provided by the user matches one or more of the letters in the word
 var checkGuesses = function() {
   var guessValue = document.guessForm.elements["letterInput"].value; // the letter provided by the user
   var inputReset = document.guessForm.elements["letterInput"];
@@ -15,6 +16,7 @@ var checkGuesses = function() {
   //deletes the guessfield and replaces it with the new one
   var guessArea = document.getElementById("guessArea");
   guessArea.innerHTML = "";
+  // prints the guessfield
   printwordArray();
 
   // if a guessed letter is not in the word, the letter will be put on the "wrong letters"-list
@@ -27,12 +29,15 @@ var checkGuesses = function() {
       guessFieldFull = false;
     }
   }
+  //checks for winner
   winner(guessFieldFull);
 
-  //once you got six wrong letters, you lose
-  looser(wrongGuess);
+  //checks for loser
+  //once you get six wrong letters, you lose
+  loser(wrongGuess);
 };
 
+// if a guessed letter is not in the word, the letter will be put on the "wrong letters"-list and also lowers tries counter
 function userLetterGuess(userGuess, guessValue) {
   if (!userGuess) {
     var generateLetter = document.getElementById("generateLetter");
@@ -43,23 +48,26 @@ function userLetterGuess(userGuess, guessValue) {
     document.querySelector(".tries").innerHTML = triesLeft;
   }
 }
-
+//checks for winner
 function winner(guessFieldFull) {
   if (guessFieldFull) {
     M.toast({ html: "You Win!", classes: "rounded" });
   }
 }
-
-function looser(wrongGuess) {
+//checks for loser
+//once you get six wrong letters, you lose
+function loser(wrongGuess) {
   if (wrongGuess === 6) {
     M.toast({ html: "Voldemort killed you", classes: "rounded" });
   }
 }
 
+//reloads the page to restart the game
 function newGame() {
   location.reload();
 }
 
+//initializes the game
 window.onload = printwordArray;
 
 //event listeners
--- js/words.js
+++ js/words.js
@@ -1,41 +1,42 @@
+//word bank for the bot to choose from
 var wordBank = [
-    "HARRY",
-    "RON",
-    "HERMIONE",
-    "VOLDEMORT",
-    "RUBEUS",
-    "DOBBY",
-    "BELLATRIX",
-    "LUNA",
-    "ALBUS",
-    "SEVERUS",
-    "WEASLEY",
-    "GRINDELWALD",
-    "NYMPHADORA",
-    "MCGONAGALL",
-    "SCAMANDER"
-  ];
-  
-  function random(word) {
-    return word[Math.floor(Math.random(word) * word.length)];
-  }
-  
-  var randomWord = random(wordBank);
-  
-  var wordArray = randomWord.split("");
-  console.log(wordArray);
-  var wrongGuess = 0;
-  
-  // every letter in the word is symbolized by an underscore in the guessfield
+  "HARRY",
+  "RON",
+  "HERMIONE",
+  "VOLDEMORT",
+  "RUBEUS",
+  "DOBBY",
+  "BELLATRIX",
+  "LUNA",
+  "ALBUS",
+  "SEVERUS",
+  "WEASLEY",
+  "GRINDELWALD",
+  "NYMPHADORA",
+  "MCGONAGALL",
+  "SCAMANDER"
+];
+//gets a random word from the word bank
+function random(word) {
+  return word[Math.floor(Math.random(word) * word.length)];
+}
+//creates a variable to call the random function
+var randomWord = random(wordBank);
+//splits the randomly generated word
+var wordArray = randomWord.split("");
+console.log(wordArray);
+var wrongGuess = 0;
+
+// every letter in the word is symbolized by an underscore in the guessfield
+for (var i = 0; i < wordArray.length; i++) {
+  wordArray[i] = "_ ";
+}
+
+// prints the guessfield
+function printwordArray() {
   for (var i = 0; i < wordArray.length; i++) {
-    wordArray[i] = "_ ";
+    var guessArea = document.getElementById("guessArea");
+    var letter = document.createTextNode(wordArray[i]);
+    guessArea.appendChild(letter);
   }
-  
-  // prints the guessfield
-  function printwordArray() {
-    for (var i = 0; i < wordArray.length; i++) {
-      var guessArea = document.getElementById("guessArea");
-      var letter = document.createTextNode(wordArray[i]);
-      guessArea.appendChild(letter);
-    }
-  }
+}
